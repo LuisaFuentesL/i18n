83c00569c76c2c5fb584a691d2c0d1abb546b9d9
Luisa <l.fuentesl@uniandes.edu.co>
2022-11-22 20:53:00 -0500

    No funcionó el encabezado puse  la tabla ENG negra

--- src/components/jobslist.js
+++ src/components/jobslist.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import Job from "./job";
+import {FormattedMessage} from 'react-intl';
+
 
 const JobsList = () => {
   const [offers] = useState([
@@ -10,6 +12,7 @@ const JobsList = () => {
       salary: 4.5,
       city: "Bogotá, Colombia",
       date: "2019-03-26",
+      views: 29000,
     },
     {
       id: "0002",
@@ -18,6 +21,7 @@ const JobsList = () => {
       salary: 20,
       city: "Palo Alto, CA, USA",
       date: "2019-03-27",
+      views: 168000,
     },
     {
       id: "0003",
@@ -26,31 +30,86 @@ const JobsList = () => {
       salary: 1,
       city: "Cali, Colombia",
       date: "2019-03-28",
+      views: 435000,
     },
   ]);
+  const lenguaje = window.navigator.language
+  if (lenguaje=== 'es-ES'){
+    return (
+      <div>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">
+                <FormattedMessage id="Position"/>
+              </th>
+              <th scope="col">
+                <FormattedMessage id="Company"/>
+              </th>
+              <th scope="col">
+                <FormattedMessage id="Salary"
+                />
+              </th>
+              <th scope="col">
+                <FormattedMessage id="City"/>
+              </th>
+              <th scope="col">
+                <FormattedMessage id="PublicationDate"/>
+              </th>
+              <th scope="col">
+                <FormattedMessage id="Views"/>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {console.log("Offers", offers)}
+            {offers.map((e, i) => (
+              <Job key={i} offer={e}/>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 
+else{
   return (
     <div>
-      <table className="table">
+      <table className="table table-bordered table-dark">
         <thead className="thead-dark">
           <tr>
             <th scope="col">#</th>
-            <th scope="col">Position</th>
-            <th scope="col">Company</th>
-            <th scope="col">Salary</th>
-            <th scope="col">City</th>
-            <th scope="col">Publication date</th>
+            <th scope="col">
+              <FormattedMessage id="Position"/>
+            </th>
+            <th scope="col">
+              <FormattedMessage id="Company"/>
+            </th>
+            <th scope="col">
+              <FormattedMessage id="Salary"
+              />
+            </th>
+            <th scope="col">
+              <FormattedMessage id="City"/>
+            </th>
+            <th scope="col">
+              <FormattedMessage id="PublicationDate"/>
+            </th>
+            <th scope="col">
+              <FormattedMessage id="Views"/>
+            </th>
           </tr>
         </thead>
         <tbody>
           {console.log("Offers", offers)}
           {offers.map((e, i) => (
-            <Job key={i} offer={e} />
+            <Job key={i} offer={e}/>
           ))}
         </tbody>
       </table>
     </div>
   );
-};
-
+}
+}
 export default JobsList;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {IntlProvider} from 'react-intl';
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 
-import JobsList from "./components/jobslist";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+import JobsList from "./components/jobslist"
+alert (window.navigator.language);
+if (window.navigator.language === 'es-ES') {
+    ReactDOM.render(
+        
+        <IntlProvider locale="es-ES" messages= {localeEsMessages}>
+                <JobsList/>
+        </IntlProvider>, document.getElementById("root")
+    );}
+else{
+    ReactDOM.render(
+        
+        <IntlProvider locale="en-US" messages= {localeEnMessages}>
+                <JobsList/>
+        </IntlProvider>, document.getElementById("root")
+    );  
+}
